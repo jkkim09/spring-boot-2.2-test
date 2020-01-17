9945fc9123be0be813c94f229667e5fa5e5c5a9f
jakyoungkim <jakyoung.kim@catenoid.net>
2020-01-17 10:37:10 +0900

    react spring hello react

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,19 +1,29 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class HomeController {
 
+	@RequestMapping("/")
+	public ModelAndView index() {
+		System.out.println("/index");
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("key", "INDEX");
+		return mv;
+	}
+	
 	@RequestMapping("/test")
 	public String home() {
+		System.out.println("/test");
 		return "Hello World! jk test";
 	}
 	
 	@RequestMapping("/main")
 	public ModelAndView main() {
+		System.out.println("/main");
 		ModelAndView mv = new ModelAndView("main");
 		mv.addObject("key", "JKKIM");
 		return mv;
