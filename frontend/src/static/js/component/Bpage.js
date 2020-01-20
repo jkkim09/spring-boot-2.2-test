7f4ae89c43070d8853c46de5cf1cb33b057cf569
jakyoungkim <jakyoung.kim@catenoid.net>
2020-01-20 17:11:09 +0900

    component import

--- frontend/src/static/js/component/Bpage.js
+++ frontend/src/static/js/component/Bpage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Dpage from './Dpage';
 
 class Bpage extends Component {
     render() {
         return (
             <div>
                 Hello Bpage
+                <Dpage></Dpage>
             </div>
         );
     }
---
+++ frontend/src/static/js/component/Dpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dpage extends Component {
+    render() {
+        return (
+            <div>
+                Hello Dpage
+            </div>
+        );
+    }
+}
+
+export default Dpage;
